5ea98059b16f32ec75f28462b3881ee4f0de5e15
vikash <vikashkisku@gmail.com>
2016-01-06 16:47:48 +0530

    corrected the shuffling part and tested correctly

--- _includes/call_break.php
+++ _includes/call_break.php
@@ -1,14 +1,17 @@
-<?PHP 
-
+<?PHP
 class call_break{
 		private $cards=array();
 		private $black_sparrow=array();
-		private $spads=array();
+		private $spaids=array();
 		private $diamond=array();
 		private $hearts=array();
+		private $player1=array();
+		private $player2=array();
+		private $player3=array();
+		private $player4=array();
 function __construct(){
 		self::cards();
-		$this->cards=self::add_to_array($this->black_sparrow,$this->spads,$this->diamond,$this->hearts);
+		$this->cards=self::add_to_array($this->black_sparrow,$this->spaids,$this->diamond,$this->hearts);
 }
 function start(){
 		self::divide_to_players(self::shuffle_assoc($this->cards));
@@ -82,14 +85,32 @@ function cards(){
 				"BQ"=>10,
 				"BK"=>11,
 				"BA"=>12
+	);$this->spaids=array(
+				"S2"=>0,
+				"S3"=>1,
+				"S4"=>2,
+				"S5"=>3,
+				"S6"=>4,
+				"S7"=>5,
+				"S8"=>6,
+				"S9"=>7,
+				"S10"=>8,
+				"SJ"=>9,
+				"SQ"=>10,
+				"SK"=>11,
+				"SA"=>12
 	);
-
 }
 function divide_to_players($cards){
+$players=array();
 		if(count($cards)!=52)
 		return false;
 			$players=array(array());
-		print_r(array_chunk($cards,13,true));
+		$players=array_chunk($cards,13,true);
+		$this->player1=$players[0];
+		$this->player2=$players[1];
+		$this->player3=$players[2];
+		$this->player4=$players[3];
 }
 
 function shuffle_assoc($array){
@@ -109,9 +130,27 @@ function shuffle_assoc($array){
     // Return
         return $shuffled_array;
 	}
-
+function get_cards($option=null){
+		if($option==null){
+			return  $this->hearts+$this->diamond+$this->spaids+$this->black_sparrow;
+			
+		}
+		else if($option=="H" || $option=="h")return $this->hearts;
+		else if($option=="D" || $option=="d")return $this->diamond;
+		else if($option=="S" || $option=="s")return $this->spaids;
+		else if($option=="B" || $option=="b")return $this->black_sparrow;
+		else return false;
+	}
+	function get_players($player_no=null){
+			if($player_no==null)return false;
+			if($player_no==1)return $this->player1;
+			if($player_no==2)return $this->player2;
+			if($player_no==3)return $this->player3;
+			if($player_no==4)return $this->player4;
+	}
 }
-$ob=new call_break();
-$ob->start();
-
+//$ob=new call_break();
+//$ob->start();
+//;
+ //$ob->start();
 ?>
--- demo/index.php
+++ demo/index.php
@@ -1,5 +1,14 @@
 <?PHP 
 require_once('../_includes/call_break.php');
 $ob=new call_break();
-print_r($ob->start());
+$ob->start();
+echo "Player 1 </br>";
+print_r($ob->get_players(1));
+echo "</br></br>Player 2</br> ";
+print_r($ob->get_players(2));
+echo "</br></br>Player 3</br>";
+print_r($ob->get_players(3));
+echo "</br></br>Player 4</br> ";
+print_r($ob->get_players(4));
+
 ?>
