9e2862024bdcae68b8304eb2539ff77e340e3d3b
vikash <vikashkisku@gmail.com>
2016-01-09 07:34:46 +0530

    assembling of the cards made more compack using associative array

--- _includes/call_break.php
+++ _includes/call_break.php
@@ -1,105 +1,37 @@
 <?PHP
 class call_break{
 		private $cards=array();
-		private $black_sparrow=array();
-		private $spaids=array();
-		private $diamond=array();
-		private $hearts=array();
+		//private $black_sparrow=array();
+		//private $spaids=array();
+		//private $diamond=array();
+		//private $hearts=array();
 		private $player1=array();
 		private $player2=array();
 		private $player3=array();
 		private $player4=array();
 function __construct(){
 		self::cards();
-		$this->cards=self::add_to_array($this->black_sparrow,$this->spaids,$this->diamond,$this->hearts);
+		//$this->cards=self::add_to_array($this->black_sparrow,$this->spaids,$this->diamond,$this->hearts);
 }
 function start(){
 		self::divide_to_players(self::shuffle_assoc($this->cards));
 }
 
-function add_to_array($array1=null,$array2=null,$array3=null,$array4=null){
-		$new_array=array();
-			if($array1!=null){
-				foreach($array1 as $key=>$val){
-					$new_array[$key]=$val;
-				}
-			}
-			if($array2!=null){
-				foreach($array2 as $key=>$val){
-					$new_array[$key]=$val;
-				}
-			}
-			if($array3!=null){
-				foreach($array3 as $key=>$val){
-					$new_array[$key]=$val;
-				}
-			}
-			if($array4!=null){
-				foreach($array4 as $key=>$val){
-					$new_array[$key]=$val;
-				}
-			}
-		return $new_array;
-}
 function cards(){
+$suits=array("Hearts","Diamond","Clubs","Spades");
+$faces=array("Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King","Ace");
+$values=array(1,2,3,4,5,6,7,8,9,10,11,12,13);
 
-	$this->hearts=array(
-				"H2"=>0,
-				"H3"=>1,
-				"H4"=>2,
-				"H5"=>3,
-				"H6"=>4,
-				"H7"=>5,
-				"H8"=>6,
-				"H9"=>7,
-				"H10"=>8,
-				"HJ"=>9,
-				"HQ"=>10,
-				"HK"=>11,
-				"HA"=>12
-	);    $this->diamond=array(
-				"D2"=>0,
-				"D3"=>1,
-				"D4"=>2,
-				"D5"=>3,
-				"D6"=>4,
-				"D7"=>5,
-				"D8"=>6,
-				"D9"=>7,
-				"D10"=>8,
-				"DJ"=>9,
-				"DQ"=>10,
-				"DK"=>11,
-				"DA"=>12
-	);  $this->black_sparrow=array(
-				"B2"=>0,
-				"B3"=>1,
-				"B4"=>2,
-				"B5"=>3,
-				"B6"=>4,
-				"B7"=>5,
-				"B8"=>6,
-				"B9"=>7,
-				"B10"=>8,
-				"BJ"=>9,
-				"BQ"=>10,
-				"BK"=>11,
-				"BA"=>12
-	);$this->spaids=array(
-				"S2"=>0,
-				"S3"=>1,
-				"S4"=>2,
-				"S5"=>3,
-				"S6"=>4,
-				"S7"=>5,
-				"S8"=>6,
-				"S9"=>7,
-				"S10"=>8,
-				"SJ"=>9,
-				"SQ"=>10,
-				"SK"=>11,
-				"SA"=>12
-	);
+foreach($suits as $suit ){
+	foreach($faces as $value=>$face){
+		
+		$this->cards[]=array("face"=>$face,"suit"=>$suit,"value"=>$value+2);
+	}
+}
+	
+}
+function get_cards(){
+return $this->cards;
 }
 function divide_to_players($cards){
 $players=array();
@@ -130,7 +62,7 @@ function shuffle_assoc($array){
     // Return
         return $shuffled_array;
 	}
-function get_cards($option=null){
+function get_card($option=null){
 		if($option==null){
 			return  $this->hearts+$this->diamond+$this->spaids+$this->black_sparrow;
 			
@@ -141,16 +73,35 @@ function get_cards($option=null){
 		else if($option=="B" || $option=="b")return $this->black_sparrow;
 		else return false;
 	}
-	function get_players($player_no=null){
-			if($player_no==null)return false;
-			if($player_no==1)return $this->player1;
-			if($player_no==2)return $this->player2;
-			if($player_no==3)return $this->player3;
-			if($player_no==4)return $this->player4;
+function get_players($player_no=null){
+		if($player_no==null)return false;
+		if($player_no==1)return $this->player1;
+		if($player_no==2)return $this->player2;
+		if($player_no==3)return $this->player3;
+		if($player_no==4)return $this->player4;
 	}
 }
-//$ob=new call_break();
+$ob=new call_break();
+$deck=$ob->get_cards();
+$ob->start();
+$player=$ob->get_players(2);
+
 //$ob->start();
 //;
  //$ob->start();
 ?>
+  <table  border="2" style="width:30%">
+  <tr>
+    <th>Card</th>
+    <th>Type</th>
+    <th>Value</th>
+  </tr>
+  <?PHP
+		foreach($deck as $cards){
+  ?>
+  <tr>
+    &nbsp;<td><?=$cards['face'];?></td>
+    <td><?=$cards['suit'];?></td>
+    <td><?=$cards['value'];}?></td>
+  </tr>
+</table> 
